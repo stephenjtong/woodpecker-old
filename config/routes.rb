5e323f2b2bfade08ac6d4dccbdd02486f3cadd04
Stephen <stephen.j.tong@gmail.com>
2013-03-17 22:41:04 +0800

    add page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rss::Application.routes.draw do
+  get "page/index"
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Rss::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'page#index'
 
   # See how all your routes lay out with "rake routes"
 

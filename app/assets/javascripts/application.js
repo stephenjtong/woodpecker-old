bede94151d8c49603cd791d962053d2f79bd0fd4
Stephen <stephen.j.tong@gmail.com>
2013-03-20 22:02:48 +0800

    install backbone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/woodpecker
 //= require_tree .
